08af7f634afc744532001fd49983a44655bd8ffd
Fredaobob <lokgamer01@gmail.com>
2024-08-18 13:42:00 -0300

    fixed some shit, finished the ''landnig page''

--- src/App.js
+++ src/App.js
@@ -11,39 +11,31 @@ function App() {
           <p>serviços</p>
         </div>
 
-        <div className='headerCenterDiv'>
-          <h1>front-end. web(developer)_</h1>
+        {/* Centered Text */}
+        <div className='headerCenterWrapper'>
+          <div className='headerCenterDiv'>
+            <h1>front-end. web(developer)_</h1>
+          </div>
         </div>
 
         <div className='headerNavBar'>
           <p><a>Home</a></p>
           <p><a>cases</a></p>
           <p><a>projects</a></p>
-          <p><a>aboute me</a></p>
+          <p><a>about me</a></p>
           <p><a>contact</a></p>
         </div>
 
         <div className='modeToggle'>
           <p>light mode</p>
         </div>
-
       </section>
 
-      <section className='cases'>
-
-      </section>
-      <section className='projects'>
-
-      </section>
-      <section className='about'>
-
-      </section>
-      <section className='contact'>
-
-      </section>
-      <section className='footer'>
-
-      </section>
+      <section className='cases'></section>
+      <section className='projects'></section>
+      <section className='about'></section>
+      <section className='contact'></section>
+      <section className='footer'></section>
     </div>
   );
 }
