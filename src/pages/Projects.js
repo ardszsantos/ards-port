05aeee17273d52b1b5082b28e899305f8d0515b2
Fredaobob <lokgamer01@gmail.com>
2024-09-08 12:37:30 -0300

    feat: tried another pŕojects section

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import '../styles/projects.css';
+import ProjectCard from '../components/projectCard';
 
 
 
 
 function Projects() {
   return (
-    <div className='projectsWrapper'>
-      
+
+    <div className='projects'>
+      <div className='FourProjectsWrapper'>
+        <ProjectCard imgUrl={`${process.env.PUBLIC_URL}/foodi1.png`} title={'Foodi'} subtitle={'Restaurant landing page'}/>
+        <ProjectCard imgUrl={`${process.env.PUBLIC_URL}/foodi1.png`} title={'Foodi'} subtitle={'Restaurant landing page'} />
+        <ProjectCard imgUrl={`${process.env.PUBLIC_URL}/foodi1.png`} title={'Foodi'} subtitle={'Restaurant landing page'} />
+        <ProjectCard imgUrl={`${process.env.PUBLIC_URL}/foodi1.png`} title={'Foodi'} subtitle={'Restaurant landing page'} />
+      </div>
+      <div className='FourProjectsWrapper'>
+        <ProjectCard imgUrl={`${process.env.PUBLIC_URL}/foodi1.png`} title={'Foodi'} subtitle={'Restaurant landing page'} />
+        <ProjectCard imgUrl={`${process.env.PUBLIC_URL}/foodi1.png`} title={'Foodi'} subtitle={'Restaurant landing page'} />
+        <ProjectCard imgUrl={`${process.env.PUBLIC_URL}/foodi1.png`} title={'Foodi'} subtitle={'Restaurant landing page'} />
+        <ProjectCard imgUrl={`${process.env.PUBLIC_URL}/foodi1.png`} title={'Foodi'} subtitle={'Restaurant landing page'} />
+      </div>
     </div>
   );
 }
